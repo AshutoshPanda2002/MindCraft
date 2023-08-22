6e1fc145a9001544b6e26796365c6e481e2485fa
Ashutosh Panda <ashutoshpanda2002@gmail.com>
2023-08-22 08:55:33 +0530

    constructed initial class in Battle_NPC.py

--- Battle_NPC.py
+++ Battle_NPC.py
@@ -2,3 +2,12 @@
 
 from transformers import pipeline
 classifier = pipeline("zero-shot-classification")
+
+class battle_npc:
+
+    def __init__(self) -> None:
+        pass
+
+    def command(self, command: str):
+        candidate_labels = ["Attack", "Follow Me", "Fallback"]
+        classifier(command, candidate_labels)
